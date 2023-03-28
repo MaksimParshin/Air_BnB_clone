0962634ff778053c045ef12713d8708e8f2a3c5c
MaksimParshin <mak.yp@yandex.ru>
2023-03-28 19:51:55 +0300

    fix

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,9 @@
-import group from "../images/Group.png";
+
 
 export default function Hero() {
   return (
     <section className="hero">
-      <img className="hero__img" src={group} alt="human" />
+      <img className="hero__img" src="../images/Group.png" alt="human" />
 
       <h1 className="hero__title">Online Experiences</h1>
       <p className="hero__description">
